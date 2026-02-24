8e9b953d2d880e57d16800ef4c1e1805a75d6138
rifuki <rifuki.dev@gmail.com>
2026-02-25 01:30:52 +0700

    feat(nvim): add JSON LSP schemas and package-info plugin
    - Configure jsonls with schemastore for tsconfig, package.json, eslint, prettier
    - Add package-info.nvim for inline npm package version display
    - Add keymaps <leader>ns/c/u/U for package management

--- shared/.config/nvim/lua/configs/lsp/defaults.lua
+++ shared/.config/nvim/lua/configs/lsp/defaults.lua
@@ -1,3 +1,48 @@
+-- JSON LSP with schemas for tsconfig.json, package.json, etc.
+vim.lsp.config("jsonls", {
+    settings = {
+        json = {
+            schemas = {
+                -- TypeScript config schemas
+                {
+                    fileMatch = { "tsconfig.json", "tsconfig.*.json" },
+                    url = "https://json.schemastore.org/tsconfig.json",
+                },
+                -- Package.json schema
+                {
+                    fileMatch = { "package.json" },
+                    url = "https://json.schemastore.org/package.json",
+                },
+                -- ESLint config schemas
+                {
+                    fileMatch = { ".eslintrc.json", ".eslintrc" },
+                    url = "https://json.schemastore.org/eslintrc.json",
+                },
+                -- Prettier config
+                {
+                    fileMatch = { ".prettierrc", ".prettierrc.json", "prettier.config.json" },
+                    url = "https://json.schemastore.org/prettierrc.json",
+                },
+                -- VSCode settings
+                {
+                    fileMatch = { ".vscode/settings.json" },
+                    url = "https://json.schemastore.org/vscode-settings.json",
+                },
+                -- Next.js config
+                {
+                    fileMatch = { "next.config.json" },
+                    url = "https://json.schemastore.org/next.json",
+                },
+            },
+            validate = { enable = true },
+            completion = true,
+            hover = true,
+            -- Use schema store for everything else (auto-detect)
+            schemaDownload = { enable = true },
+        },
+    },
+})
+
 -- Default LSP servers with minimal config
 vim.lsp.enable({ "lua_ls", "dockerls", "bashls", "jsonls" })
 
--- shared/.config/nvim/lua/plugins/init.lua
+++ shared/.config/nvim/lua/plugins/init.lua
@@ -7,4 +7,42 @@ return {
             require("configs.lspconfig")
         end,
     },
+    {
+        "vuki656/package-info.nvim",
+        event = { "BufRead package.json" },
+        dependencies = { "MunifTanjim/nui.nvim" },
+        config = function()
+            require("package-info").setup({
+                autostart = true,
+                hide_up_to_date = false,
+                package_manager = "npm",
+                icons = {
+                    enable = true,
+                    style = {
+                        up_to_date = "| ",
+                        outdated = "| ",
+                        invalid = "| ",
+                    },
+                },
+                -- Use default highlight groups (no custom colors)
+                -- The plugin will use its internal defaults
+            })
+
+            -- Override highlight colors after setup
+            vim.cmd([[
+                highlight PackageInfoUpToDate guifg=#3C4048
+                highlight PackageInfoOutdated guifg=#FCA5A5
+                highlight PackageInfoInvalid guifg=#EE4B2B
+            ]])
+
+            -- Keymaps
+            local map = vim.keymap.set
+            local pi = require("package-info")
+
+            map("n", "<leader>ns", pi.show, { desc = "Show package versions", silent = true })
+            map("n", "<leader>nc", pi.hide, { desc = "Hide package versions", silent = true })
+            map("n", "<leader>nu", pi.update, { desc = "Update package on line", silent = true })
+            map("n", "<leader>nU", pi.change_version, { desc = "Change package version", silent = true })
+        end,
+    },
 }
