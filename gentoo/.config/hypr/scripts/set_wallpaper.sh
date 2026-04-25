0424a7b10cd5408342944ab0f22b4b6fdb037607
rifuki <rifuki.dev@gmail.com>
2026-04-26 03:47:32 +0700

    Fix Rifuki wallpaper controls

--- gentoo/.config/hypr/scripts/set_wallpaper.sh
+++ gentoo/.config/hypr/scripts/set_wallpaper.sh
@@ -5,7 +5,7 @@ wallpaper_name="${1:-}"
 [ -n "$wallpaper_name" ] || exit 1
 
 if [ -f "$wallpaper_name" ]; then
-  exec rifuki-wallpaper-set "$wallpaper_name"
+  exec "$HOME/.local/bin/rifuki-wallpaper-set" "$wallpaper_name"
 fi
 
-exec rifuki-wallpaper-set "$HOME/.wallpapers/$wallpaper_name"
+exec "$HOME/.local/bin/rifuki-wallpaper-set" "$HOME/.wallpapers/$wallpaper_name"
